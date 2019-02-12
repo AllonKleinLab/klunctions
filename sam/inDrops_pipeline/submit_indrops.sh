c56ece5fea2fb8fac01e87924cbd8ac5e87613b8
swolock <swolock@gmail.com>
2019-02-12 13:48:39 -0500

    fixed bug in indrops.py submission script

--- sam/inDrops_pipeline/submit_indrops.sh
+++ sam/inDrops_pipeline/submit_indrops.sh
@@ -30,26 +30,25 @@ myPython=/n/groups/klein/sam/pyndrops/bin/python
 source ${pyenv}/bin/activate ${pyenv}
 
 
-### PART 1 (filter)
-## Use this if just one set of fastq files
-#jid1=$( sbatch -p short --array 1-${N1} --job-name F --mem 6000 -t 12:00:00 -o "$logdir"filter_worker_%a.out -e "$logdir"filter_worker_%a.out --wrap """ ${myPython} ${indrops} $yaml filter --total-workers ${N1} --worker-index \$((\$SLURM_ARRAY_TASK_ID-1)) """ )
-#jid1=${jid1##* }
-#echo $jid1
-#
-### PART 2 (identify abundant barcodes) 
-#jid2=$( sbatch --dependency=afterany:$jid1 -p short --job-name I --mem 2000 -t 1:00:00 -o "$logdir"abundant.out -e "$logdir"abundant.out --wrap """ ${myPython} $indrops $yaml identify_abundant_barcodes """ )
-#jid2=${jid2##* }
-#echo $jid2
-#
-### PART 3 (sort)
-## max number of workers = number of libraries
-#jid3=$( sbatch --dependency=afterany:$jid2 -p short --array 1-${N3} --job-name S --mem 16000 -t 12:00:00 -o "$logdir"sort_worker_%a.out -e "$logdir"sort_worker_%a.out --wrap """ ${myPython} $indrops $yaml sort --total-workers ${N3} --worker-index \$((\$SLURM_ARRAY_TASK_ID-1)) """ )
-#jid3=${jid3##* }
-#echo $jid3
+## PART 1 (filter)
+# Use this if just one set of fastq files
+jid1=$( sbatch -p short --array 1-${N1} --job-name F --mem 6000 -t 12:00:00 -o "$logdir"filter_worker_%a.out -e "$logdir"filter_worker_%a.out --wrap """ ${myPython} ${indrops} $yaml filter --total-workers ${N1} --worker-index \$((\$SLURM_ARRAY_TASK_ID-1)) """ )
+jid1=${jid1##* }
+echo $jid1
 
-# PART 4 (quantify)
-#jid4=$( sbatch --dependency=afterany:$jid3 -p short --array 1-${N4} --job-name Q --mem 3000 -t 06:00:00 -o "$logdir"quant_worker_%a.out -e "$logdir"quant_worker_%a.out --wrap """ ${myPython} $indrops $yaml quantify --min-reads 250 --min-counts 0 --total-workers ${N4} --worker-index \$((\$SLURM_ARRAY_TASK_ID-1)) --no-bam """)
-jid4=$( sbatch -p short --array 1-${N4} --job-name Q --mem 3000 -t 06:00:00 -o "$logdir"quant_worker_%a.out -e "$logdir"quant_worker_%a.out --wrap """ ${myPython} $indrops $yaml quantify --min-reads 250 --min-counts 0 --total-workers ${N4} --worker-index \$((\$SLURM_ARRAY_TASK_ID-1)) --no-bam """)
+## PART 2 (identify abundant barcodes) 
+jid2=$( sbatch --dependency=afterany:$jid1 -p short --job-name I --mem 2000 -t 1:00:00 -o "$logdir"abundant.out -e "$logdir"abundant.out --wrap """ ${myPython} $indrops $yaml identify_abundant_barcodes """ )
+jid2=${jid2##* }
+echo $jid2
+
+## PART 3 (sort)
+# max number of workers = number of libraries
+jid3=$( sbatch --dependency=afterany:$jid2 -p short --array 1-${N3} --job-name S --mem 16000 -t 12:00:00 -o "$logdir"sort_worker_%a.out -e "$logdir"sort_worker_%a.out --wrap """ ${myPython} $indrops $yaml sort --total-workers ${N3} --worker-index \$((\$SLURM_ARRAY_TASK_ID-1)) """ )
+jid3=${jid3##* }
+echo $jid3
+
+## PART 4 (quantify)
+jid4=$( sbatch --dependency=afterany:$jid3 -p short --array 1-${N4} --job-name Q --mem 3000 -t 06:00:00 -o "$logdir"quant_worker_%a.out -e "$logdir"quant_worker_%a.out --wrap """ ${myPython} $indrops $yaml quantify --min-reads 250 --min-counts 0 --total-workers ${N4} --worker-index \$((\$SLURM_ARRAY_TASK_ID-1)) --no-bam """)
 jid4=${jid4##* }
 echo $jid4
 
